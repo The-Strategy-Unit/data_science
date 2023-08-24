58fa23bbb7a567ae2eebf671c99026b18e10631b
Tom Jemmett <thomas.jemmett@nhs.net>
2023-08-24 13:51:13 +0100

    cleans up the action, adds script for generating the dependencies

---
+++ .github/workflows/rebuild_system_dependencies.r
@@ -0,0 +1,43 @@
+# in the rebuild_site action there is a list of system dependencies that need
+# to be installed. the following script uses posit public package manager to
+# find the system dependencies for the installed packages in renv
+
+# hopefully one day setup-renv will do this for us...
+
+library(tidyverse)
+library(jsonlite)
+library(httr)
+
+packages <- read_json("renv.lock") |>
+  _$Packages |>
+  names() |>
+  map(\(.x) {
+    httr::GET(
+      "https://packagemanager.rstudio.com",
+      path = c(
+        "__api__",
+        "repos",
+        "1",
+        "sysreqs"
+      ),
+      query = list(
+        all = "false",
+        distribution = "ubuntu",
+        release = "22.04",
+        pkgname = .x
+      )
+    ) |>
+      httr::content() |>
+      _$requirements |>
+      map(pluck, "requirements", "install_scripts") |>
+      purrr::flatten() |>
+      flatten_chr()
+  }) |>
+  flatten_chr() |>
+  unique() |>
+  sort() |>
+  str_remove("^apt-get install -y ") |>
+  paste(collapse = " ")
+
+# copy the results of this into the action script
+paste("sudo apt update && sudo apt upgrade", packages)
